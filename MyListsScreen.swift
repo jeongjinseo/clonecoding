b48e535d004e816064443504ce3fcdd2bc8e730b
jeongjinseo <jjs041126@khu.ac.kr>
2025-10-01 22:49:42 +0900

    Create MyListsScreen.swift

---
+++ MyListsScreen.swift
@@ -0,0 +1,28 @@
+impor SwiftUI
+
+struct MyListScreen: View {
+    let myLists = ["Reminders", "Groceries", "Entertaniment"]
+
+    var body: some View {
+        List {
+            Text("My Lists")
+                .font(.largeTitle)
+                .bold()
+
+            ForEach(myLists, id: \.self) { myList in
+                HStack {
+                    Image(systemName: "line.3.horizontal.circle.fill")
+                        .font(.system(size: 32))
+                    Text(myList)
+                } 
+            }
+        }.listStyle(.plain)
+    }
+}
+
+
+#Preview {
+    NavigationStack {
+        MyListsScreen()
+    }
+}
